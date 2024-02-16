8c95bd4e2209ff5c06c10f36d02def6d951244c1
soup-hacker <miles.campbell@mymail.champlain.edu>
2024-02-16 14:54:48 -0500

    update milestone 4

--- vm-linked-clone.ps1
+++ vm-linked-clone.ps1
@@ -1,14 +1,13 @@
 $vcenter="vcenter-480.campbell.local"
 $vm_name="ubuntu"
-$new_vm_name="server.ubuntu.base"
+$new_vm_network="480-WAN"
+
 
 Connect-VIServer -Server $vcenter
 $vm = Get-VM -Name $vm_name
-$snapshot = Get-Snapshot -VM $vm -Name "base"
 $vmhost=Get-VMHost -Name "192.168.7.14"
 $ds = Get-DataStore -Name "datastore1-super4"
-$linkedClone = "{0}.linked" -f $vm.name
+$linkedClone = "awx"
+$snapshot = Get-Snapshot -VM $vm -Name "base"
 $linkedvm = New-VM -LinkedClone -Name $linkedClone -VM $vm -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $ds
-$newvm = New-VM -Name $new_vm_name -VM $linkedvm -VMHost $vmhost -Datastore $ds
-$newvm | New-Snapshot -Name "Base"
-$linkedvm | Remove-VM
+$linkedvm | Get-NetworkAdapter | Set-NetworkAdapter -NetworkName $new_vm_network
